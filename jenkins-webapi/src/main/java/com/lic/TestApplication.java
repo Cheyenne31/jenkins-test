9e95af073d1ef5d8f1ddd8e396566b9a4dd806df
lichen <lichen@szhcf.com.cn>
2019-08-26 10:54:12 +0800

    ces

---
+++ jenkins-webapi/src/main/java/com/lic/TestApplication.java
@@ -0,0 +1,21 @@
+package com.lic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * <PRE>
+ *
+ * </PRE>
+ *
+ * @author :25808-lic
+ * @date :2019/8/26,10:19
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class,args);
+    }
+}
---
+++ jenkins-webapi/src/main/java/com/lic/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.lic.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <PRE>
+ *
+ * </PRE>
+ *
+ * @author :25808-lic
+ * @date :2019/8/26,10:19
+ */
+@Api(tags = "小程序-排班信息")
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @ApiOperation(value = "jenkins测试", notes = "")
+    @PostMapping("/jenkins")
+    public Boolean listMonthSchedule() {
+        return Boolean.TRUE;
+    }
+}
